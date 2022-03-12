b5efc5309ed61966af589208ba1ab91b1a3ed3ca
Laurent <lyenpon974@gmail.com>
2022-03-12 02:49:19 +0100

    Update - Instal config nvim

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -72,6 +72,7 @@ npm install -g yarn
 
 echo '${RED}Installation OhMyZSH'
 cp .zshrc ~/
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 chsh -s $(which zsh)
 source .zshrc
