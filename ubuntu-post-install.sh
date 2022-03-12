4986f3678e355aec4556e6b21b7917302d42d3b0
Laurent <lyenpon974@gmail.com>
2022-03-12 02:06:39 +0100

    Update - Instal config nvim

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -23,42 +23,42 @@ apt-get install neovim -y
 apt install curl-y
 
 # Make config directory for Neovim's init.vim
-echo '${RED}[*] Installation de nvim'
+echo '${RED}[*] Installation de nvim${NC}'
 mkdir -p ~/.config/nvim
 
 # Install vim-plug plugin manager
-echo '${RED}[*] Téléchargement de vim-plug'
+echo '${RED}[*] Téléchargement de vim-plug${NC}'
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Create a backup of your current coc-settings.json if you have one
 if [ -f ~/.config/nvim/coc-settings.json ]; then
-  echo '${RED}[*] Backup existing coc-settings.json'
+  echo '${RED}[*] Backup existing coc-settings.json${NC}'
   cp ~/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json.backup
 fi
 
 # Copy coc-settings.json in current working directory to nvim's config location. This is for custom diagnostic signs
-echo '${RED}[*] Copying .config folder .config -> ~/.config'
+echo '${RED}[*] Copying .config folder .config -> ~/.config${NC}'
 cp .config/nvim/plug-config/coc.vim ~/.config/nvim/plug-config/
 
 # Create a backup of your current init.vim if you have one
 if [ -f ~/.config/nvim/init.vim ]; then
-  echo '${RED}[*] Backup existing init.vim'
+  echo '${RED}[*] Backup existing init.vim${NC}'
   cp ~/.config/nvim/init.vim ~/.config/nvim/init.vim.backup
 fi
 
 # Enter Neovim and install plugins with vim-plug's :PlugInstall and coc extensions with CoC's :CocInstall using a temporary init.vim, which avoids warnings about missing colorschemes, functions, etc
-echo -e '${RED}[*] Running :PlugInstall within nvim ...'
+echo -e '${RED}[*] Running :PlugInstall within nvim ...${NC}'
 sed '/call plug#end/q' .config/nvim/init.vim > ~/.config/nvim/init.vim
 nvim -c 'PlugInstall' -c 'qa'
 rm ~/.config/nvim/init.vim
 
 # Copy init.vim in current working directory to nvim's config location
-echo '${RED}[*] Copying init.vim -> ~/.config/nvim/init.vim'
+echo '${RED}[*] Copying init.vim -> ~/.config/nvim/init.vim${NC}'
 cp .config/nvim/init.vim ~/.config/nvim/
 
-echo -e "${RED}[+] Installation de neovim terminé."
+echo -e "${RED}[+] Installation de neovim terminé.${NC}"
 
 # Install nodejs
-echo '[*] Installation de nodejs'
+echo '[*] Installation de nodejs${NC}'
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
 apt install -y nodejs
