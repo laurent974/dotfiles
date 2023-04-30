3539e691f88c0b82c713cfc97d742a29ae8cd98f
Laurent Yen-Pon <lyenpon974@gmail.com>
2023-04-30 20:30:00 +0200

    Add - nvim install

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
+read -n1 -rep $'[\e[1;33mACTION\e[0m] - Est-ce que tu veux installer NVIM? [y/n] ' NVIM 
+if [[ $NVIM == "Y" || $NVIM == "y" || $NVIM == "" ]]; then
+  bash "$PWD"/install/nvim.sh
+fi
+
 read -n1 -rep $'[\e[1;33mACTION\e[0m] - Est-ce que tu veux installer TMUX? [y/n] ' TMUX 
 if [[ $TMUX == "Y" || $TMUX == "y" || $TMUX == "" ]]; then
   bash "$PWD"/install/tmux.sh
